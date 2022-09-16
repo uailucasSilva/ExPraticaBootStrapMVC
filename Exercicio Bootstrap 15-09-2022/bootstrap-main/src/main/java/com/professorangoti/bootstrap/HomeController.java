6530477d752b02157eab7bbe4f7ad66afa4ddfdd
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-09-15 21:56:15 -0300

    Add files via upload
    Etapa 2

--- Exercicio Bootstrap 15-09-2022/bootstrap-main/src/main/java/com/professorangoti/bootstrap/HomeController.java
+++ Exercicio Bootstrap 15-09-2022/bootstrap-main/src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -1,7 +1,13 @@
 package com.professorangoti.bootstrap;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.micrometer.core.instrument.util.IOUtils;
 
 @Controller
 public class HomeController {
